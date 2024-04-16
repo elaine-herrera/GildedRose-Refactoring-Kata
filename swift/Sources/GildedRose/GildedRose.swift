4deb1fa1674dfe31a9d8bbe896b625467cb90946
Elaine Herrera <elainehdelcueto@gmail.com>
2024-04-16 22:25:02 +0200

    Refactoring Sulfuras.

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -52,23 +52,19 @@ public class GildedRose {
             
             return
         }
-        else {
-            if item.quality > 0 {
-                if item.name != "Sulfuras, Hand of Ragnaros" {
-                    item.quality = item.quality - 1
-                }
-            }
+        if item.name == "Sulfuras, Hand of Ragnaros" {
+            return
         }
         
-        if item.name != "Sulfuras, Hand of Ragnaros" {
-            item.sellIn = item.sellIn - 1
+        if item.quality > 0 {
+            item.quality = item.quality - 1
         }
+        
+        item.sellIn = item.sellIn - 1
         
         if item.sellIn < 0 {
             if item.quality > 0 {
-                if item.name != "Sulfuras, Hand of Ragnaros" {
-                    item.quality = item.quality - 1
-                }
+                item.quality = item.quality - 1
             }
         }
     }
