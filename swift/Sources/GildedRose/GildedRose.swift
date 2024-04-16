f766d192bb2adfb5a788dde081eec17613d7675d
Elaine Herrera <elainehdelcueto@gmail.com>
2024-04-16 21:56:37 +0200

    Reverting if a != b with if a == b for readability

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -12,13 +12,7 @@ public class GildedRose {
     }
     
     private func updateQuality(item: Item) {
-        if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
-            if item.quality > 0 {
-                if item.name != "Sulfuras, Hand of Ragnaros" {
-                    item.quality = item.quality - 1
-                }
-            }
-        } else {
+        if item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert"{
             if item.quality < 50 {
                 item.quality = item.quality + 1
 
@@ -37,13 +31,25 @@ public class GildedRose {
                 }
             }
         }
+        else {
+            if item.quality > 0 {
+                if item.name != "Sulfuras, Hand of Ragnaros" {
+                    item.quality = item.quality - 1
+                }
+            }
+        }
 
         if item.name != "Sulfuras, Hand of Ragnaros" {
             item.sellIn = item.sellIn - 1
         }
 
         if item.sellIn < 0 {
-            if item.name != "Aged Brie" {
+            if item.name == "Aged Brie" {
+                if item.quality < 50 {
+                    item.quality = item.quality + 1
+                }
+            }
+            else {
                 if item.name != "Backstage passes to a TAFKAL80ETC concert" {
                     if item.quality > 0 {
                         if item.name != "Sulfuras, Hand of Ragnaros" {
@@ -52,10 +58,6 @@ public class GildedRose {
                     }
                 } else {
                     item.quality = item.quality - item.quality
-                }
-            } else {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1
                 }
             }
         }
