ecf755ac746950671ef122616ec36f8fbc5c1cba
Elaine Herrera <elainehdelcueto@gmail.com>
2024-04-16 22:26:44 +0200

    Using && to join nested ifs

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -19,10 +19,8 @@ public class GildedRose {
             
             item.sellIn = item.sellIn - 1
             
-            if item.sellIn < 0 {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1
-                }
+            if item.sellIn < 0 && item.quality < 50 {
+                item.quality = item.quality + 1
             }
             
             return
@@ -31,16 +29,12 @@ public class GildedRose {
             if item.quality < 50 {
                 item.quality = item.quality + 1
                 
-                if item.sellIn < 11 {
-                    if item.quality < 50 {
-                        item.quality = item.quality + 1
-                    }
+                if item.sellIn < 11 && item.quality < 50 {
+                    item.quality = item.quality + 1
                 }
                 
-                if item.sellIn < 6 {
-                    if item.quality < 50 {
-                        item.quality = item.quality + 1
-                    }
+                if item.sellIn < 6 && item.quality < 50 {
+                    item.quality = item.quality + 1
                 }
             }
             
@@ -62,10 +56,8 @@ public class GildedRose {
         
         item.sellIn = item.sellIn - 1
         
-        if item.sellIn < 0 {
-            if item.quality > 0 {
-                item.quality = item.quality - 1
-            }
+        if item.sellIn < 0 && item.quality > 0 {
+            item.quality = item.quality - 1
         }
     }
 }
