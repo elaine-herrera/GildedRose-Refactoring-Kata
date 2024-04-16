34274af66e4f3a91f2e87f5d3a81385688401096
Elaine Herrera <elainehdelcueto@gmail.com>
2024-04-16 22:16:32 +0200

    Refactoring Aged Brie case, applying early return

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -1,10 +1,10 @@
 public class GildedRose {
     var items: [Item]
-
+    
     public init(items: [Item]) {
         self.items = items
     }
-
+    
     public func updateQuality() {
         for i in 0 ..< items.count {
             updateQuality(item: items[i])
@@ -16,18 +16,28 @@ public class GildedRose {
             if item.quality < 50 {
                 item.quality = item.quality + 1
             }
+            
+            item.sellIn = item.sellIn - 1
+            
+            if item.sellIn < 0 {
+                if item.quality < 50 {
+                    item.quality = item.quality + 1
+                }
+            }
+            
+            return
         }
         else if item.name == "Backstage passes to a TAFKAL80ETC concert"{
             if item.quality < 50 {
                 item.quality = item.quality + 1
-
+                
                 if item.name == "Backstage passes to a TAFKAL80ETC concert" {
                     if item.sellIn < 11 {
                         if item.quality < 50 {
                             item.quality = item.quality + 1
                         }
                     }
-
+                    
                     if item.sellIn < 6 {
                         if item.quality < 50 {
                             item.quality = item.quality + 1
@@ -43,26 +53,19 @@ public class GildedRose {
                 }
             }
         }
-
+        
         if item.name != "Sulfuras, Hand of Ragnaros" {
             item.sellIn = item.sellIn - 1
         }
-
+        
         if item.sellIn < 0 {
-            if item.name == "Aged Brie" {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1
-                }
+            if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                item.quality = item.quality - item.quality
             }
             else {
-                if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                    item.quality = item.quality - item.quality
-                }
-                else {
-                    if item.quality > 0 {
-                        if item.name != "Sulfuras, Hand of Ragnaros" {
-                            item.quality = item.quality - 1
-                        }
+                if item.quality > 0 {
+                    if item.name != "Sulfuras, Hand of Ragnaros" {
+                        item.quality = item.quality - 1
                     }
                 }
             }
