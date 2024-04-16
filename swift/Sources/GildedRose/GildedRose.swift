180e89b184aa836d20a416d83da8fa2c26315ef1
Elaine Herrera <elainehdelcueto@gmail.com>
2024-04-16 22:20:01 +0200

    Refactoring Backstage case, applying early return

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -27,24 +27,30 @@ public class GildedRose {
             
             return
         }
-        else if item.name == "Backstage passes to a TAFKAL80ETC concert"{
+        if item.name == "Backstage passes to a TAFKAL80ETC concert"{
             if item.quality < 50 {
                 item.quality = item.quality + 1
                 
-                if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                    if item.sellIn < 11 {
-                        if item.quality < 50 {
-                            item.quality = item.quality + 1
-                        }
+                if item.sellIn < 11 {
+                    if item.quality < 50 {
+                        item.quality = item.quality + 1
                     }
-                    
-                    if item.sellIn < 6 {
-                        if item.quality < 50 {
-                            item.quality = item.quality + 1
-                        }
+                }
+                
+                if item.sellIn < 6 {
+                    if item.quality < 50 {
+                        item.quality = item.quality + 1
                     }
                 }
             }
+            
+            item.sellIn = item.sellIn - 1
+            
+            if item.sellIn < 0 {
+                item.quality = item.quality - item.quality
+            }
+            
+            return
         }
         else {
             if item.quality > 0 {
@@ -59,14 +65,9 @@ public class GildedRose {
         }
         
         if item.sellIn < 0 {
-            if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                item.quality = item.quality - item.quality
-            }
-            else {
-                if item.quality > 0 {
-                    if item.name != "Sulfuras, Hand of Ragnaros" {
-                        item.quality = item.quality - 1
-                    }
+            if item.quality > 0 {
+                if item.name != "Sulfuras, Hand of Ragnaros" {
+                    item.quality = item.quality - 1
                 }
             }
         }
